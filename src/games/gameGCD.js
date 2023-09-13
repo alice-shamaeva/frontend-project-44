7065e6afeec058e24eef426f8ac6ecb3bfac5c7e
alice-shamaeva <alice.shamaeva@gmail.com>
2023-09-13 19:02:07 +0400

    correction of an inadvertent error

--- src/games/gameGCD.js
+++ src/games/gameGCD.js
@@ -20,7 +20,7 @@ const startGCD = () => {
   const randomNumber2 = getRandomNumber(0, 101);
   const question = `${randomNumber1} ${randomNumber2}`;
   const answer = getNOD(randomNumber1, randomNumber2);
-  return [String(question), String(answer)];
+  return [Number(question), String(answer)];
 };
 
 const startRoundGCD = () => {
