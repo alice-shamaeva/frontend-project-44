6607a0898151ecf358112667669e6edff4cfc0da
alice-shamaeva <alice.shamaeva@gmail.com>
2023-09-13 19:05:30 +0400

    stupid bug in GCD

--- src/games/gameGCD.js
+++ src/games/gameGCD.js
@@ -18,9 +18,9 @@ const getNOD = (num1, num2) => {
 const startGCD = () => {
   const randomNumber1 = getRandomNumber(0, 101);
   const randomNumber2 = getRandomNumber(0, 101);
-  const question = `${randomNumber1} ${randomNumber2}`;
+  const question = `${Number(randomNumber1)} ${Number(randomNumber2)}`;
   const answer = getNOD(randomNumber1, randomNumber2);
-  return [Number(question), String(answer)];
+  return [(question), String(answer)];
 };
 
 const startRoundGCD = () => {
